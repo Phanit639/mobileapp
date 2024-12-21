2dfacf909889d9d91035084d9a5ffe318498e528
Phanit639 <phanit639@gmail.com>
2024-12-21 14:45:13 +0700

    update file main.dart, homepage.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:demoproject/homepage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,6 +8,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,79 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'CPSU Project'), // เปลี่ยน title ที่นี่
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red, // พื้นหลังสีแดง
-        title: Text(
-          widget.title,
-          style: const TextStyle(color: Colors.white), // ตัวอักษรสีขาว
-        ),
-        centerTitle: true, // จัดให้อยู่กึ่งกลาง
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center, // จัดให้อยู่กึ่งกลาง
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(20),
-              margin: const EdgeInsets.only(bottom: 10),
-              color: Colors.blueAccent, // พื้นหลังของ container
-              child: const Text(
-                'Hello World 1',
-                style: TextStyle(color: Colors.white, fontSize: 20),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(20),
-              margin: const EdgeInsets.only(bottom: 10),
-              color: Colors.greenAccent, // พื้นหลังของ container
-              child: const Text(
-                'Hello World 2',
-                style: TextStyle(color: Colors.black, fontSize: 20),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(20),
-              margin: const EdgeInsets.only(bottom: 10),
-              color: Colors.purpleAccent, // พื้นหลังของ container
-              child: const Text(
-                'Hello World 3',
-                style: TextStyle(color: Colors.white, fontSize: 20),
-              ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: const MyHomePage(),
     );
   }
 }
