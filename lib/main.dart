97295966f4346b48f86ede512261f68cb9ea9428
Phanit639 <phanit639@gmail.com>
2024-12-21 16:00:11 +0700

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:demoproject/homepage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,11 +12,37 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Flutter Demo',
+            style: TextStyle(
+              color: Colors.red),
+        ),
+        leading: const Icon(Icons.add),
+        backgroundColor: Colors.green,
+        ),
+        body: 
+        Container(
+          height: 100,
+          width: 100,
+          color: Colors.orange,
+          padding: const EdgeInsets.all(20),
+          margin: const EdgeInsets.all(20),
+          child: const Text('Hello'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print("AI NONG");
+          },
+          child: const Icon(Icons.phone),
+          ),
+      )
     );
   }
 }
